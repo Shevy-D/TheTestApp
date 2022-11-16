34694ea1a69a99ea3cf8466864969d98077b70ff
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-11-16 14:39:35 +0500

    I added the splash screen

--- app/src/main/java/com/shevy/thetestapp/MainActivity.kt
+++ app/src/main/java/com/shevy/thetestapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.shevy.thetestapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.shevy.thetestapp.databinding.ActivityMainBinding
@@ -9,8 +10,15 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        Thread.sleep(2000)
+        setTheme(R.style.Theme_TheTestApp)
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
-        //setContentView(R.layout.activity_main)
+
+        binding.button.setOnClickListener {
+            startActivity(Intent(this, SplashActivity::class.java))
+        }
     }
 }
