234467fa26f6b4156326c92595ca900ef264e7ca
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-11-18 19:40:04 +0500

    Added the GridView for the Best Seller

---
+++ app/src/main/java/com/shevy/thetestapp/BestSellerGridViewAdapter.kt
@@ -0,0 +1,100 @@
+package com.shevy.thetestapp
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Paint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.shevy.thetestapp.model.BestSeller
+
+
+class BestSellerGridViewAdapter(
+    private val context: Context,
+    private val bestSellerList: ArrayList<BestSeller>
+) :
+    BaseAdapter() {
+
+    override fun getCount(): Int {
+        return bestSellerList.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return bestSellerList[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    @SuppressLint("ViewHolder", "SetTextI18n")
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View? {
+
+        val itemBestSeller = bestSellerList[position]
+
+        val view = LayoutInflater.from(context)
+            .inflate(R.layout.best_seller_container, parent, false) as View
+
+        val bestSellerView: ImageView = view.findViewById(R.id.image_best_seller)
+        val title: TextView = view.findViewById(R.id.title_best_seller)
+        val discount: TextView = view.findViewById(R.id.price_with_discount)
+        val price: TextView = view.findViewById(R.id.price_without_discount)
+        val favorite: ImageView = view.findViewById(R.id.image_favorite)
+
+        title.text = itemBestSeller.title
+        price.text = "$${itemBestSeller.discount_price}"
+        discount.apply {
+            this.text = "$${itemBestSeller.price_without_discount}"
+            discount.paintFlags = this.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+        }
+        Glide.with(bestSellerView.context)
+            .load(itemBestSeller.picture)
+            .into(bestSellerView)
+
+        if (itemBestSeller.is_favorites) {
+            favorite.setImageResource(R.drawable.ic_heart_filled)
+        }
+
+        return view
+    }
+}
+
+/*
+class ProductAdapter(context: Context, products: List<Product>) :
+    BaseAdapter() {
+    private val context: Context
+    private val products: List<Product>
+    override fun getCount(): Int {
+        return products.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return products[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val button: Button
+        if (convertView == null) {
+            button = Button(context)
+            button.setText(products[position].getName())
+        } else {
+            button = convertView as Button
+        }
+        button.setId(position)
+        return button
+    }
+
+    init {
+        this.context = context
+        this.products = products
+    }
+}
+*/
--- app/src/main/java/com/shevy/thetestapp/MainActivity.kt
+++ app/src/main/java/com/shevy/thetestapp/MainActivity.kt
@@ -4,22 +4,27 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.GridView
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
-import com.bumptech.glide.Glide.init
 import com.shevy.thetestapp.databinding.ActivityMainBinding
+import com.shevy.thetestapp.model.BestSeller
 import com.shevy.thetestapp.model.HomeStore
 
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
-
-    private lateinit var adapter: HotSalesAdapter
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var hotSalesAdapter: HotSalesAdapter
+    private lateinit var bestSellerAdapter: BestSellerAdapter
+    private lateinit var bestSellerGridViewAdapter: BestSellerGridViewAdapter
     private lateinit var viewPager: ViewPager2
-    private lateinit var handler : Handler
+    private lateinit var handler: Handler
     private lateinit var product: ArrayList<HomeStore>
+    private lateinit var bestSellerGridView: GridView
 
     @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,42 +35,102 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
 
-        initAll()
+        initAdapterHotSales()
+        initRecyclerViewBestSeller()
     }
 
-    private fun initAll() {
+    private fun initRecyclerViewBestSeller() {
+
+        bestSellerGridView = binding.bestSellerRv
+        val bestSellerList = ArrayList<BestSeller>()
+
+        bestSellerList.add(
+            BestSeller(
+                1500,
+                111,
+                true,
+                "https://shop.gadgetufa.ru/images/upload/52534-smartfon-samsung-galaxy-s20-ultra-12-128-chernyj_1024.jpg",
+                1047,
+                "Samsung Galaxy s20 Ultra"
+            )
+        )
+        bestSellerList.add(
+            BestSeller(
+                400,
+                222,
+                true,
+                "https://mi92.ru/wp-content/uploads/2020/03/smartfon-xiaomi-mi-10-pro-12-256gb-global-version-starry-blue-sinij-1.jpg",
+                300,
+                "Xiaomi Mi 10 Pro"
+            )
+        )
+        bestSellerList.add(
+            BestSeller(
+                1500,
+                333,
+                false,
+                "https://opt-1739925.ssl.1c-bitrix-cdn.ru/upload/iblock/c01/c014d088c28d45b606ed8c58e5817172.jpg?160405904823488",
+                1047,
+                "Samsung Note 20 Ultra"
+            )
+        )
+        bestSellerList.add(
+            BestSeller(
+                400,
+                4444,
+                true,
+                "https://www.benchmark.rs/assets/img/news/edge1.jpg",
+                300,
+                "Motorola One Edge "
+            )
+        )
+
+        bestSellerGridViewAdapter = BestSellerGridViewAdapter(this@MainActivity, bestSellerList)
+        bestSellerGridView.adapter = bestSellerGridViewAdapter
+    }
+
+    private fun initAdapterHotSales() {
         viewPager = binding.viewPager
         handler = Handler(Looper.myLooper()!!)
         product = ArrayList()
 
-        product.add(HomeStore(1,
-            is_buy = true,
-            is_new = true,
-            picture = "https://img.ibxk.com.br/2020/09/23/23104013057475.jpg?w=1120&h=420&mode=crop&scale=both",
-            subtitle = "Súper. Mega. Rápido.",
-            title = "Iphone 12"
-        ))
+        product.add(
+            HomeStore(
+                1,
+                is_buy = true,
+                is_new = true,
+                picture = "https://img.ibxk.com.br/2020/09/23/23104013057475.jpg?w=1120&h=420&mode=crop&scale=both",
+                subtitle = "Súper. Mega. Rápido.",
+                title = "Iphone 12"
+            )
+        )
 
-        product.add(HomeStore(2,
-            is_buy = true,
-            is_new = false,
-            picture = "https://cdn-2.tstatic.net/kupang/foto/bank/images/pre-order-samsung-galaxy-a71-laris-manis-inilah-rekomendasi-ponsel-harga-rp-6-jutaan.jpg",
-            subtitle = "Súper. Mega. Rápido.",
-            title = "Samsung Galaxy A71"
-        ))
+        product.add(
+            HomeStore(
+                2,
+                is_buy = true,
+                is_new = false,
+                picture = "https://cdn-2.tstatic.net/kupang/foto/bank/images/pre-order-samsung-galaxy-a71-laris-manis-inilah-rekomendasi-ponsel-harga-rp-6-jutaan.jpg",
+                subtitle = "Súper. Mega. Rápido.",
+                title = "Samsung Galaxy A71"
+            )
+        )
 
-        product.add(HomeStore(3,
-            is_buy = true,
-            is_new = false,
-            picture = "https://static.digit.in/default/942998b8b4d3554a6259aeb1a0124384dbe0d4d5.jpeg",
-            subtitle = "Súper. Mega. Rápido.",
-            title = "Xiaomi Mi 11 ultra"
-        ))
+        product.add(
+            HomeStore(
+                3,
+                is_buy = true,
+                is_new = false,
+                picture = "https://static.digit.in/default/942998b8b4d3554a6259aeb1a0124384dbe0d4d5.jpeg",
+                subtitle = "Súper. Mega. Rápido.",
+                title = "Xiaomi Mi 11 ultra"
+            )
+        )
 
-        adapter = HotSalesAdapter(product, viewPager)
+        hotSalesAdapter = HotSalesAdapter(product, viewPager)
 
 
-        viewPager.adapter = adapter
+        viewPager.adapter = hotSalesAdapter
         viewPager.run {
             offscreenPageLimit = 3
             clipToPadding = false
